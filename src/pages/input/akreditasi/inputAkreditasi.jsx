63466be348370654e70cb106e6ae88c0acd1d532
Tamakuz <jovangithub@gmail.com>
2023-09-08 11:26:04 +0700

    REFAC: Crud AKreditasi

--- src/pages/input/akreditasi/inputAkreditasi.jsx
+++ src/pages/input/akreditasi/inputAkreditasi.jsx
@@ -1,9 +1,20 @@
-import AkreditasiBab1 from "../../../components/input/akreditasiBab1";
+import { Flex } from "@chakra-ui/react";
+import Bab1 from "../../../components/akreditasi/bab1";
+import Bab2 from "../../../components/akreditasi/bab2";
 import Sidebar from "../../../layout/sidebar";
+import Bab3 from "../../../components/akreditasi/bab3";
 
 const InputAkreditasi = () => {
   const breadcrumbs = ["Input Data", "Akreditasi"];
-  return <Sidebar breadcrumbs={breadcrumbs}><AkreditasiBab1 /></Sidebar>;
+  return (
+    <Sidebar breadcrumbs={breadcrumbs}>
+      <Flex className="flex-col gap-2">
+        <Bab1 />
+        <Bab2 />
+        <Bab3 />
+      </Flex>
+    </Sidebar>
+  );
 };
 
 export default InputAkreditasi;
