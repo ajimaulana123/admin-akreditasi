a2d9f435a01c9b0de02657926657ccf8dd92c7db
Tamakuz <jovangithub@gmail.com>
2023-09-09 13:10:51 +0700

    REFAC:Style Akreditasi

--- src/components/inputAkreditasi/bab1/d.jsx
+++ src/components/inputAkreditasi/bab1/d.jsx
@@ -41,7 +41,7 @@ const D = () => {
     <Accordion allowMultiple defaultIndex={[0]}>
       <AccordionItem
         className={`${
-          colorMode === "dark" ? "bg-secondaryGray-900" : "bg-white"
+          colorMode === "dark" ? "bg-secondaryGray-900" : "bg-secondaryGray-300"
         } border-none rounded-xl shadow`}
       >
         <Text>
