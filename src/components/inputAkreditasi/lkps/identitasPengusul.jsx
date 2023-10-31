937867efa8fc0fdf31f3ce005aa46cbeef6abb09
Tamakuz <jovangithub@gmail.com>
2023-10-31 16:11:34 +0700

    REFAC: Poin LKPS

--- src/components/inputAkreditasi/lkps/identitasPengusul.jsx
+++ src/components/inputAkreditasi/lkps/identitasPengusul.jsx
@@ -1,17 +1,7 @@
-import {
-  Accordion,
-  AccordionButton,
-  AccordionIcon,
-  AccordionItem,
-  AccordionPanel,
-  Text,
-  useColorMode,
-} from "@chakra-ui/react";
 import React from "react";
 import DTamplate from "../bab1/Component/dTamplate";
 
 const IdentitasPengusul = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
   return <DTamplate endpoint="identitas-pengusul" title="Identitas Pengusul" />;
 };
 
